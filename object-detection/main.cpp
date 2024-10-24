a44fb4878aeef34134e535b9cf64ec77ab55f73e
Machive5 <machiblue5@gmail.com>
2024-10-24 21:25:36 +0700

    fix

--- object-detection/main.cpp
+++ object-detection/main.cpp
@@ -6,10 +6,10 @@ using namespace cv;
 using namespace std;
 
 double calcDistance(float r){
-    double hasil = 0;
+    double result = 0;
 
-    hasil = (1.5132851736838930*pow(10,2))+(-2.1225457381657278*r)+(1.4383492118754407*pow(10,-2)*pow(r,2))+(-4.7379848523256183*pow(10,-5)*pow(r,3))+(6.1464904018921641*pow(10,-8)*pow(r,4));
-    return hasil;
+    result = (1.5132851736838930*pow(10,2))+(-2.1225457381657278*r)+(1.4383492118754407*pow(10,-2)*pow(r,2))+(-4.7379848523256183*pow(10,-5)*pow(r,3))+(6.1464904018921641*pow(10,-8)*pow(r,4));
+    return result;
 }
 
 int main(){
@@ -23,7 +23,7 @@ int main(){
     int v[2] = {0,255};
     int area = 15;
 
-    namedWindow("trackbar");
+    //namedWindow("trackbar");
 
     createTrackbar("h min", "trackbar", &h[0], 255);
     createTrackbar("s min", "trackbar", &s[0], 255);
@@ -54,6 +54,7 @@ int main(){
                 minEnclosingCircle(contours[i], center[i], radius[i]);
 
                 if (radius[i]>area){
+                    //cout<<radius[i]<<endl;
                     Point tpos(static_cast<int>(center[i].x-radius[i]), static_cast<int>(center[i].y+radius[i]));
                     circle(frame, center[i], cvRound(radius[i]), Scalar(0,255,0), 2);
                     putText(frame, to_string(calcDistance(radius[i])) + " cm", tpos, QT_FONT_NORMAL, 1, Scalar(0,0,255), 2);
@@ -63,6 +64,7 @@ int main(){
         }
         
         imshow("video", frame);
+        //imshow("thres", thres);
         
         if (waitKey(1) == 'q') {
             break;
